1bbb3b79bc85a76a444d477b4d1bbd0de57a7587
Cliff Conolly <cliffconolly@protonmail.com>
2019-08-01 11:46:56 +0100

    remove hammer

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <img className="lightning" src='assets/lightning.png'/>
       <img className="lightning2" src='assets/lightning2.png'/>
       <img className="lightning3" src='assets/lightning3.png'/>
-      <img className="thors_hammer" src='assets/Thors_Hammer.png'/>
+      {/* <img className="thors_hammer" src='assets/Thors_Hammer.png'/> */}
       <p className="chariots">Chariots of the gods</p>
       <p className="revo">The Electric Revolution Is Upon Us</p>
     </div>
